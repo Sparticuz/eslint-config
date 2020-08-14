6c679a1faf8cfd05ff40fac5852dd3b04f33c64d
Kyle McNally <kyle@kmcnally.net>
2020-08-14 15:39:49 -0400

    Rebuild to lint both Javascript files, and Typescript files

--- eslint.config.js
+++ eslint.config.js
@@ -2,65 +2,92 @@ module.exports = {
   env: {
     es6: true,
     es2017: true,
+    es2020: true,
     node: true,
   },
   extends: [
     "airbnb-base",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:import/errors",
     "plugin:import/warnings",
-    "plugin:import/typescript",
     "plugin:promise/recommended",
     "plugin:node/recommended",
     "plugin:unicorn/recommended",
     "prettier",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
   ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
+  // Ignore my dist folders so I don't lint transpiled files
   ignorePatterns: "dist/**/*",
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      impliedStrict: true,
+  overrides: [
+    {
+      // Test Javascript Files Override Block
+      files: ["**/tests/*.js"],
+      rules: {
+        "node/no-unpublished-require": 0,
+      },
     },
-    ecmaVersion: 2019,
-    project: "tsconfig.json",
+    {
+      // Typescript Override Block
+      extends: [
+        "airbnb-base",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "plugin:promise/recommended",
+        "plugin:node/recommended",
+        "plugin:unicorn/recommended",
+        "prettier",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint",
+      ],
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: "@typescript-eslint-parser",
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+      plugins: [
+        "@typescript-eslint",
+        "typescript-sort-keys",
+        "import",
+        "node",
+        "prettier",
+        "promise",
+        "unicorn",
+      ],
+      rules: {
+        // Make sure we don't require .ts and .tsx
+        "import/extensions": [
+          "error",
+          "ignorePackages",
+          {
+            ts: "never",
+            tsx: "never",
+          },
+        ],
+        // Typescript uses Imports for transpiling
+        "node/no-unsupported-features/es-syntax": [
+          "error",
+          {
+            ignores: ["modules"],
+          },
+        ],
+      },
+      settings: {
+        node: {
+          tryExtensions: [".ts"],
+        },
+      },
+    },
+  ],
+  parser: "esprima",
+  parserOptions: {
+    ecmaVersion: 2020,
     sourceType: "module",
   },
-  plugins: [
-    "@typescript-eslint",
-    "typescript-sort-keys",
-    "import",
-    "node",
-    "prettier",
-    "promise",
-    "unicorn",
-  ],
+  plugins: ["import", "node", "prettier", "promise", "unicorn"],
   rules: {
-    // Make sure we don't require .ts and .tsx
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
-      },
-    ],
-    // Allow aws-sdk to exist in optionalDependencies and be used in production
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        optionalDependencies: true,
-      },
-    ],
     // disallow certain syntax forms, allows for-of
     // http://eslint.org/docs/rules/no-restricted-syntax
     "no-restricted-syntax": [
@@ -69,13 +96,6 @@ module.exports = {
       "LabeledStatement",
       "WithStatement",
     ],
-    // Typescript uses Imports for transpiling
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      {
-        ignores: ["modules"],
-      },
-    ],
     "sort-keys": [
       "error",
       "asc",
@@ -83,21 +103,5 @@ module.exports = {
         natural: true,
       },
     ],
-    "typescript-sort-keys/interface": 2,
-    "typescript-sort-keys/string-enum": 2,
-  },
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
-    },
-    "import/resolver": {
-      // use <root>/tsconfig.json
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
-    node: {
-      tryExtensions: [".js", ".json", ".ts"],
-    },
   },
 };
