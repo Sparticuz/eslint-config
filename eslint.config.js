155785eed9040c4e9491ab0025134a61077bd3e5
Kyle McNally <kyle@kmcnally.net>
2024-04-03 09:42:43 -0400

    add globals and cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -7,18 +7,19 @@ import nodePlugin from "eslint-plugin-n";
 import unicornPlugin from "eslint-plugin-unicorn";
 import arrayFuncPlugin from "eslint-plugin-array-func";
 import * as regexpPlugin from "eslint-plugin-regexp";
-// @ts-ignore Missing types?
+// @ts-ignore https://github.com/azat-io/eslint-plugin-perfectionist/issues/90
 import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural'
+import globals from "globals";
 
 /**
- * Plugins to re-implement
+ *  Plugins to re-implement when they support Flat Config
  * "eslint-plugin-deprecation": https://github.com/gund/eslint-plugin-deprecation/pull/79,
  * "eslint-plugin-eslint-comments": https://github.com/eslint-community/eslint-plugin-eslint-comments/pull/200
  * "eslint-plugin-no-unsanitized": https://github.com/mozilla/eslint-plugin-no-unsanitized/issues/234,
  * "eslint-plugin-promise": https://github.com/eslint-community/eslint-plugin-promise/issues/449
  * "eslint-plugin-sonarjs": https://github.com/SonarSource/eslint-plugin-sonarjs/issues/454
  *
- * Keep? Ava, Import
+ *  Keep? Ava, Import
  */
 
 export default tseslint.config(
@@ -34,12 +35,10 @@ export default tseslint.config(
   perfectionistNatural,
   prettierConfig,
   {
-    ignores: [
-      // Ignore my dist folders so I don't lint transpiled files
-      "dist/**/*",
-    ],
     languageOptions: {
-      ecmaVersion: "latest",
+      globals: {
+        ...globals.node
+      }
     },
     linterOptions: {
       reportUnusedDisableDirectives: "warn",
@@ -57,47 +56,54 @@ export default tseslint.config(
       */
       node: {
         // Make sure we are looking for Typescript files as well
-        tryExtensions: [".js", ".json", ".node", ".ts", ".d.ts"],
+        tryExtensions: [".js", ".jsx", ".mjs", ".cjs", ".ts", ".tsx", ".json", ".d.ts",],
       },
     },
     rules: {
       // Disable import/extensions because Typescript handles this now
       // "import/extensions": "off",
-      // Disable @typescript-eslint/member-ordering because of perfectionist
+      /* Disable @typescript-eslint/member-ordering because of perfectionist */
       "@typescript-eslint/member-ordering": "off",
-      // Typescript eslint has it's own @typescript-eslint/no-shadow rule
+      /* Typescript eslint has it's own @typescript-eslint/no-shadow rule */
       "no-shadow": "off",
-      // Array Func prefers array.from, however, Unicorn prefers spread. Spread is better
+      /* Array Func prefers array.from, however, Unicorn prefers spread. Spread is better */
       "array-func/prefer-array-from": "off",
-      // Dot notation is disabled via typescript, so disable it here
+      /* Dot notation is disabled via typescript, so disable it here */
       // https://typescript-eslint.io/rules/dot-notation/
       "dot-notation": "off",
       // "eslint-comments/disable-enable-pair": "off",
       // Don't prefer the default export
       // "import/prefer-default-export": ["off"],
-      // Await shouldn't be used it a loop, but sometimes it needs to be, just warn
-      // "no-await-in-loop": ["warn"],
-      // disallow certain syntax forms, allows for-of
-      // http://eslint.org/docs/rules/no-restricted-syntax
+      /* disallow certain syntax forms, allows for-of
+       * http://eslint.org/docs/rules/no-restricted-syntax */
       "no-restricted-syntax": [
         "error",
         "ForInStatement",
         "LabeledStatement",
         "WithStatement",
       ],
-      "no-unused-vars": ["warn"],
-      // Disable @typescript-eslint/member-ordering because of perfectionist
+      /* Disable no-unused-vars because of @typescript-eslint/no-unused-vars */
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      /* Make sure nulls are last in types */
+      "perfectionist/sort-union-types": [
+        "error",
+        {
+          "nullable-last": true,
+        }
+      ],
+      /* Disable @typescript-eslint/member-ordering because of perfectionist */
       "sort-keys": "off",
-      // For database stuff, I need nulls
+      /* For database stuff, I need nulls */
       "unicorn/no-null": "off",
-      // Disable switch curly braces
+      /* Disable switch curly braces */
       "unicorn/switch-case-braces": "off",
     },
   },
   {
     files: ["tests/**/*"],
     rules: {
-      // Don't require items to be published for tests
+      /* Don't require items to be published for tests */
       "n/no-unpublished-import": "off",
     },
   },
