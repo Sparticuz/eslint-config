801d47814fe5b4eb74c5d1323c0fb0c76a48d232
Kyle McNally <kyle@kmcnally.net>
2024-08-01 11:14:44 -0400

    make sure vitest config is found properly

--- src/configs/vitest.ts
+++ src/configs/vitest.ts
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config({
   extends: [vitest.configs.recommended],
-  files: ["tests/**", "test/**"],
+  files: ["tests/**", "test/**", "vitest.config.ts"],
   languageOptions: {
     globals: {
       ...vitest.environments.env.globals,
