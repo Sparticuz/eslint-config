e4b108d4379e9804fa6d5d3e994f378e8295c2bb
Sparticuz <kyle@kmcnally.net>
2024-08-27 09:20:18 -0400

    update deps

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -2,8 +2,6 @@ import perfectionist from "eslint-plugin-perfectionist";
 import tseslint, { ConfigWithExtends } from "typescript-eslint";
 
 export default tseslint.config(
-  // @ts-expect-error The configs variable does exist
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   perfectionist.configs["recommended-natural"] as ConfigWithExtends,
   {
     rules: {
