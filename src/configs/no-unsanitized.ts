119a99ce9a51af667cd46dc135814b13df6e10dd
Sparticuz <kyle@kmcnally.net>
2024-09-30 12:24:15 -0400

    update deps

--- src/configs/no-unsanitized.ts
+++ src/configs/no-unsanitized.ts
@@ -2,5 +2,5 @@ import nounsanitized from "eslint-plugin-no-unsanitized";
 import tseslint, { type ConfigWithExtends } from "typescript-eslint";
 
 export default tseslint.config(
-  nounsanitized.configs.recommended[0] as ConfigWithExtends,
+  nounsanitized.configs.recommended as ConfigWithExtends,
 );
