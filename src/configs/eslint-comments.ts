2329149df8c2fa68e2dc808151c586c236de3ffd
Kyle McNally <kyle@kmcnally.net>
2025-10-06 11:17:46 -0400

    update eslint-comments and vitest configs

--- src/configs/eslint-comments.ts
+++ src/configs/eslint-comments.ts
@@ -1,16 +1,20 @@
-import comments from "@eslint-community/eslint-plugin-eslint-comments/configs";
-import tseslint, { type ConfigWithExtends } from "typescript-eslint";
+import type { Linter } from "eslint";
 
-export default tseslint.config(comments.recommended as ConfigWithExtends, {
-  name: "@sparticuz/eslint-config/eslint-comments.ts",
-  rules: {
-    /**
-     * The allowWholeFile option lets you allow disabling rules for the entire file while
-     * still catching "open" eslint-disable directives in the middle of a file.
-     */
-    "@eslint-community/eslint-comments/disable-enable-pair": [
-      "error",
-      { allowWholeFile: true },
-    ],
+import comments from "@eslint-community/eslint-plugin-eslint-comments/configs";
+
+export default [
+  comments.recommended,
+  {
+    name: "@sparticuz/eslint-config/eslint-comments.ts",
+    rules: {
+      /**
+       * The allowWholeFile option lets you allow disabling rules for the entire file while
+       * still catching "open" eslint-disable directives in the middle of a file.
+       */
+      "@eslint-community/eslint-comments/disable-enable-pair": [
+        "error",
+        { allowWholeFile: true },
+      ],
+    },
   },
-});
+] satisfies Linter.Config[];
--- src/configs/vitest.ts
+++ src/configs/vitest.ts
@@ -1,36 +1,33 @@
 import vitest from "@vitest/eslint-plugin";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ...vitest.configs.recommended },
-  {
-    name: "@sparticuz/eslint-config/vitest.ts",
+export default tseslint.config(vitest.configs.recommended, {
+  name: "@sparticuz/eslint-config/vitest.ts",
 
-    files: ["**/tests/**", "**/test/**", "vitest.config.ts", "**/*.test.*"],
-    languageOptions: {
-      globals: {
-        ...vitest.environments.env.globals,
-      },
-    },
-    rules: {
-      /**
-       * Vite will already be there because of vitest
-       */
-      "n/no-extraneous-import": [
-        "error",
-        {
-          allowModules: ["vite"],
-        },
-      ],
-      /**
-       * Don't require items to be published for tests
-       */
-      "n/no-unpublished-import": "off",
-    },
-    settings: {
-      vitest: {
-        typecheck: true,
-      },
+  files: ["**/tests/**", "**/test/**", "vitest.config.ts", "**/*.test.*"],
+  languageOptions: {
+    globals: {
+      ...vitest.environments.env.globals,
     },
   },
-);
+  rules: {
+    /**
+     * Vite will already be there because of vitest
+     */
+    "n/no-extraneous-import": [
+      "error",
+      {
+        allowModules: ["vite"],
+      },
+    ],
+    /**
+     * Don't require items to be published for tests
+     */
+    "n/no-unpublished-import": "off",
+  },
+  settings: {
+    vitest: {
+      typecheck: true,
+    },
+  },
+});
