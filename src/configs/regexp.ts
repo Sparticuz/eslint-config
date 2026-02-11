60c915e250e223b7a0f3137208bead5cc534564e
Sparticuz <kyle@kmcnally.net>
2026-02-10 20:02:48 -0500

    build: 📦 Update dependencies in package.json refactor: ♻️ Simplify regexp config import and export

--- src/configs/regexp.ts
+++ src/configs/regexp.ts
@@ -1,10 +1,5 @@
 import type { Linter } from "eslint";
 
-import * as regexpPlugin from "eslint-plugin-regexp";
+import regexpPlugin from "eslint-plugin-regexp";
 
-export default [
-  {
-    name: "regexp/flat/recommended",
-    ...regexpPlugin.configs["flat/recommended"],
-  },
-] satisfies Linter.Config[];
+export default [regexpPlugin.configs.recommended] as Linter.Config[];
