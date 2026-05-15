a13b060a32cb5312cf55492bd569a7e349881c62
Sparticuz <kyle@kmcnally.net>
2026-05-15 16:09:29 +0000

    fix: wrap prettier export in ConfigArray so with() works on all public exports

--- src/configs/prettier.ts
+++ src/configs/prettier.ts
@@ -2,11 +2,12 @@ import type { Linter } from "eslint";
 
 import prettierConfig from "eslint-config-prettier/flat";
 
+import { ConfigArray } from "../config-array.js";
 import { JAVASCRIPT_FILES, TYPESCRIPT_FILES } from "../file-types.js";
 
-export default [
+export default new ConfigArray([
   {
     ...prettierConfig,
     files: [...JAVASCRIPT_FILES, ...TYPESCRIPT_FILES],
   },
-] satisfies Linter.Config[];
+] satisfies Linter.Config[]);
